a6d29bc5bbc4a1fc76a3a49b5d9c8c7bd401a12a
Lalit Malviya <gehlotlalit786@gmail.com>
2024-02-18 22:07:03 +0530

    first commit

---
+++ react-basic-app/src/Button.jsx
@@ -0,0 +1,18 @@
+function handleClick(){
+    console.log("Hello");  
+}
+function handleMouseOver(){
+    console.log("Bye");
+}
+function handleDblClick(){
+    console.log("you Double click me");
+}
+export default function Button(){
+    return(
+        <div>
+            <button onClick={handleClick}>Click Me!</button>
+            <p onMouseOver={handleMouseOver}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita molestias ipsam veniam ab nobis aliquam corporis id nam? Optio, cum?</p>
+            <h1 onDoubleClick={handleDblClick}>Ss college</h1>
+        </div>
+    );
+}

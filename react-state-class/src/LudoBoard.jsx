f4e0e1bcf0cc3e8a865f87671ed0e90e1d245397
Lalit Malviya <gehlotlalit786@gmail.com>
2024-03-03 20:28:46 +0530

    Array & state

--- react-state-class/src/LudoBoard.jsx
+++ react-state-class/src/LudoBoard.jsx
@@ -2,12 +2,19 @@ import { useState } from "react"
 
 export default function LudoBoard(){
     let [moves, setMoves] = useState({blue: 0, red: 0, yellow: 0, green: 0});
+    let [arr, SetArr] = useState(["no moves"]);
 
     let updateBlue = () => {
-        setMoves((prevMoves) => {
-           return {...prevMoves, blue: prevMoves.blue +1 };  //spread object
+        // setMoves((prevMoves) => {
+        //    return {...prevMoves, blue: prevMoves.blue +1 };  //spread object
+        // });
+
+        SetArr((prevArr) => {
+            return [...prevArr, "blue moves"];
         });
+        console.log(arr);
     };
+    
     let updateYellow = () => {
         setMoves((prevMoves) => {
            return {...prevMoves, yellow: prevMoves.yellow +1 };  //spread object
@@ -26,6 +33,7 @@ export default function LudoBoard(){
     return (
         <div>
             <p>Game Begins!</p>
+            <p>{arr}</p>
             <div className="board">
                 <p>Blue moves = {moves.blue}</p>
                 <button style={{backgroundColor: "blue"}} onClick={updateBlue}>+1</button>
